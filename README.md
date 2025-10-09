70d6c4fe34a0be2dd04d18489b6246d100291096
ioannispapoud16 <ioannispapoud16@gmail.com>
2025-10-09 18:49:48 +0000

    Made the game 5 round, added some null protection and a message in case the player want to quit and a little html message i the player wants to continue he can refresh

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Simple rock paper scissors game made using java script.First to 3 wins is the winner of the game.
+Simple rock paper scissors game made using java script.First to 5 wins is the winner of the game.The game is played entirly from the console for now
--- javascript.js
+++ javascript.js
@@ -16,6 +16,8 @@ function getHumanChoice()
     while(1)
     {
         let p_choice= prompt("Choose Wisley:\nRock Paper Scissors");
+        if (p_choice==null)
+            return null;
             p_choice=p_choice.trim();
             p_choice= p_choice.charAt(0).toUpperCase() + p_choice.slice(1).toLowerCase();
         if (p_choice !='Rock' && p_choice !="Paper" && p_choice!="Scissors")
@@ -27,38 +29,70 @@ function getHumanChoice()
 
 function playRound(p_choice,c_choice)
 {
-    if(p_choice=="Rock" && c_choice=="Scissors")
+    if (p_choice==null)
+        return null;
+    else if(p_choice=="Rock" && c_choice=="Scissors")
     {
         console.log(p_choice+" beats "+c_choice+". Good job! Plus One Point for you");
-        p_score++;
+        return 1;
     }
     else if(p_choice=="Scissors" && c_choice=="Paper")
     {
         console.log(p_choice+" beats "+c_choice+". Good job! Plus One Point for you");
-        p_score++;
+        return 1;
     }
     else if(p_choice=="Paper" && c_choice=="Rock")
     {
         console.log(p_choice+" beats "+c_choice+". Good job! Plus One Point for you");
-        p_score++;
+        return 1;
     }
     else if(p_choice==c_choice)
     {
         console.log(p_choice+" and "+c_choice+" is A tie. No points for everyone");
+        return 0;
     }
     else
     {
         console.log(c_choice+" beats "+p_choice+". Plus One Point for the computer :D");
-        c_score++;
+        return 2;
     }
 }
-let p_score=0;
-let c_score=0;
 
-let p_choice=getHumanChoice();
-let c_choice=getComputerChoice();
-playRound(p_choice,c_choice);
+function playGame()
+{
+    let p_score=0;
+    let c_score=0;
+    while(1)
+        {
+            let p_choice=getHumanChoice();
+            let c_choice=getComputerChoice();
+            let result= playRound(p_choice,c_choice);
+            if (result==null)
+            {
+                console.log("QUITEER")
+                return null;
+            }
+            else if(result==1)
+            {
+                p_score++;
+            }
+            else if(result==2)
+            {
+                c_score++;
+            }
+            console.log(p_score);
+            console.log(c_score);
+            if(p_score==5)
+            {
+                console.log("Player Wins !!!");
+                return 1;
+            }
+            else if(c_score==5)
+            {
+                console.log("Computer Wins :C");
+                return 0;
+            }
+        }
+}
 
-console.log(p_score);
-console.log(c_score);
-    
+playGame();
