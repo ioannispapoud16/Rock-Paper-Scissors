b3a8904e0f01b53e1a3a08391804b81cfdd7f197
ioannispapoud16 <ioannispapoud16@gmail.com>
2025-10-24 09:55:04 +0000

    made a small change in my output text class into output text and completed all the dom manipulation methods so now the game is completly playable

--- javascript.js
+++ javascript.js
@@ -2,97 +2,127 @@ function getComputerChoice()
 {
     let num= (Math.random()*1000)%3 ;
     if(num <1)
+    {
+        document.querySelector('#computer_Choice').src = "images/paper.png";
         return "Paper";
+    }
     else if(num <2)
+    {
+        document.querySelector('#computer_Choice').src = "images/scissors.png";
         return "Scissors";
+    }
     else if(num <3)
+    {
+        document.querySelector('#computer_Choice').src = "images/rock.png";
         return "Rock";
+    }   
     else
         return "Error. Invalid choice";
 }
-
-function getHumanChoice()
+function RestartGame()
 {
-    while(1)
-    {
-        let p_choice= prompt("Choose Wisley:\nRock Paper Scissors");
-        if (p_choice==null)
-            return null;
-            p_choice=p_choice.trim();
-            p_choice= p_choice.charAt(0).toUpperCase() + p_choice.slice(1).toLowerCase();
-        if (p_choice !='Rock' && p_choice !="Paper" && p_choice!="Scissors")
-            console.log("Invalid Choice. Try again");
-        else
-            return p_choice;
+    if (playerScore == 5) {
+        alert("You Won");
+        resetGame();
+    } else if (computerScore == 5) {
+        alert("You lost");
+        resetGame();
     }
 }
 
-function playRound(p_choice,c_choice)
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    computer_Score.textContent = "Computer Score: 0";
+    player_Score.textContent = "Player Score: 0";
+    let output = document.querySelector('.output-text');
+    output.textContent = "First to 5 wins";
+    document.querySelector('#player_Choice').src = "images/mystery.png";
+    document.querySelector('#computer_Choice').src = "images/mystery.png";
+}
+
+function playRound(p_choice)
 {
+    let c_choice= getComputerChoice();
     if (p_choice==null)
         return null;
     else if(p_choice=="Rock" && c_choice=="Scissors")
     {
-        console.log(p_choice+" beats "+c_choice+". Good job! Plus One Point for you");
-        return 1;
+        let text= (p_choice+" beats "+c_choice+". Good job! Plus One Point for you");
+        playerScore++;
+        let output = document.querySelector('.output-text');
+        output.textContent=text;
+        let score = document.querySelector('#player_Score');
+        score.textContent="Player Score: "+playerScore;
+        RestartGame();
+        return 0;
     }
     else if(p_choice=="Scissors" && c_choice=="Paper")
     {
-        console.log(p_choice+" beats "+c_choice+". Good job! Plus One Point for you");
-        return 1;
+        let text= (p_choice+" beats "+c_choice+". Good job! Plus One Point for you");
+        playerScore++;
+        let output = document.querySelector('.output-text');
+        output.textContent=text;
+        let score = document.querySelector('#player_Score');
+        score.textContent="Player Score: "+playerScore;
+        RestartGame();
+        return 0;
     }
     else if(p_choice=="Paper" && c_choice=="Rock")
     {
-        console.log(p_choice+" beats "+c_choice+". Good job! Plus One Point for you");
-        return 1;
+        let text= (p_choice+" beats "+c_choice+". Good job! Plus One Point for you");
+        playerScore++;
+        let output = document.querySelector('.output-text');
+        output.textContent=text;
+        let score = document.querySelector('#player_Score');
+        score.textContent="Player Score: "+playerScore;
+        RestartGame();
+        return 0;
     }
     else if(p_choice==c_choice)
     {
-        console.log(p_choice+" and "+c_choice+" is A tie. No points for everyone");
+        let text= (p_choice+" and "+c_choice+" is A tie. No points for everyone");
+        let output = document.querySelector('.output-text');
+        output.textContent=text;
+        RestartGame();
         return 0;
     }
     else
     {
-        console.log(c_choice+" beats "+p_choice+". Plus One Point for the computer :D");
-        return 2;
+        let text= (c_choice+" beats "+p_choice+". Plus One Point for the computer :D");
+        computerScore++;
+        let output = document.querySelector('.output-text');
+        output.textContent=text;
+        let score = document.querySelector('#computer_Score');
+        score.textContent="Computer Score: "+computerScore;
+        RestartGame();
+        return 0;
     }
 }
 
-function playGame()
-{
-    let p_score=0;
-    let c_score=0;
-    while(1)
-        {
-            let p_choice=getHumanChoice();
-            let c_choice=getComputerChoice();
-            let result= playRound(p_choice,c_choice);
-            if (result==null)
-            {
-                console.log("QUITEER")
-                return null;
-            }
-            else if(result==1)
-            {
-                p_score++;
-            }
-            else if(result==2)
-            {
-                c_score++;
-            }
-            console.log(p_score);
-            console.log(c_score);
-            if(p_score==5)
-            {
-                console.log("Player Wins !!!");
-                return 1;
-            }
-            else if(c_score==5)
-            {
-                console.log("Computer Wins :C");
-                return 0;
-            }
-        }
-}
+let playerScore=0;
+let computerScore=0;
+let playerChoice = null;
 
-playGame();
+const rock_button = document.querySelector("#rock_B");
+const paper_button = document.querySelector("#paper_B");
+const scissors_button = document.querySelector("#scissors_B");
+
+// Event listeners
+rock_button.addEventListener('click', () => {
+    if (true) playerChoice = 'Rock';
+    document.querySelector('#player_Choice').src = "images/rock.png";
+    playRound(playerChoice);
+});
+
+paper_button.addEventListener('click', () => {
+    if (true) playerChoice = 'Paper';
+    document.querySelector('#player_Choice').src = "images/paper.png";
+    playRound(playerChoice);
+});
+
+scissors_button.addEventListener('click', () => {
+    if (true) playerChoice = 'Scissors';
+    document.querySelector('#player_Choice').src = "images/scissors.png";
+    playRound(playerChoice);
+});
